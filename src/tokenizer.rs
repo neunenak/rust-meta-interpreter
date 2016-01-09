4601a568679836062cef0a6bfcd0134c437c3d7d
greg <greg.shuflin@protonmail.com>
2016-01-08 23:55:58 -0800

    Start working on tokenization tests

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -127,13 +127,5 @@ mod tests {
     #[test]
     fn tokeniziation_tests() {
         let t1 = "let a = 3\n";
-        assert_eq!(format!("{:?}", tokenize(t1)),
-            "[Keyword(Let), Identifier(\"a\"), Keyword(Assign), NumLiteral(3), Newline, EOF]");
-
-        // this is intentional
-        let t2 = "a + b*c\n";
-        assert_eq!(format!("{:?}", tokenize(t2)),
-            "[Identifier(\"a\"), Identifier(\"+\"), Identifier(\"b*c\"), Newline, EOF]");
-
     }
 }
