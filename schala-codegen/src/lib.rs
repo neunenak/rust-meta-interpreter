258e813a39ad35018fa9de8e9f100231d45b00a4
greg <greg.shuflin@protonmail.com>
2018-04-30 00:35:42 -0700

    Starting to write custom derive for ProgrammingLanguageInterface

--- schala-codegen/src/lib.rs
+++ schala-codegen/src/lib.rs
@@ -1,10 +1,19 @@
 #![feature(proc_macro)]
 extern crate proc_macro;
+#[macro_use]
+extern crate quote;
+extern crate syn;
 use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn print_a_thing(_input: TokenStream) -> TokenStream {
   "println!(\"Invoked from a proc macro\");".parse().unwrap()
+}
+
+
+#[proc_macro_derive(ProgrammingLanguageInterface)]
+pub fn derive_programming_language_interface(input: TokenStream) -> TokenStream {
+  input
 }
 
 #[cfg(test)]
